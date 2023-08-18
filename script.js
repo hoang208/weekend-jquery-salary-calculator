17f35dc7565eb2cdbe033270691f0703069a28cf
Paul HoangLong <paulhoanglong@gmail.com>
2023-08-18 18:35:47 -0500

    Made required fields

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function onReady() {
 }
 //end onReady()
 
+let totalMonthly = 0;
+
 function getTheInput(event){
     event.preventDefault();
     console.log("In getTheInput function:");
@@ -23,13 +25,24 @@ function getTheInput(event){
 
     let annualSalaryInput = $('#annual-salary-input').val()
     console.log("annualSalaryInput is:", annualSalaryInput)
+    if (firstNameInput!= "" && lastNameInput != "" && idInput != "" && titleInput != "" && annualSalaryInput != "" ) {
+        $('#first-name-input').val("")
+        $('#last-name-input').val("")
+        $('#id-input').val("")
+        $('#title-input').val("")
+        $('#annual-salary-input').val("")
 
-    $('#first-name-input').val("")
-    $('#last-name-input').val("")
-    $('#id-input').val("")
-    $('#title-input').val("")
-    $('#annual-salary-input').val("")
+        $('#table-body').append(`<tr id="tableRow"><td>${firstNameInput}</td><td>${lastNameInput}</td><td >${idInput}</td><td >${titleInput}</td><td >${annualSalaryInput}</td></tr>`)
+        
 
-    $('#table-body').append(`<tr id="tableRow"><td>${firstNameInput}</td><td>${lastNameInput}</td><td >${idInput}</td><td >${titleInput}</td><td >${annualSalaryInput}</td></tr>`)
-}
+        totalMonthly+=parseInt(annualSalaryInput);
+        console.log("totalMonthly:", totalMonthly)
+        $('h5').text("Total Monthly:" + totalMonthly)
+    } else if (!isNaN(idInput) && !isNaN(annualSalaryInput)){
+        alert("Please fill out ID and annual salary with only numbers.")
+    }
+    else {
+        alert("Please fill out all input fields.")
+    }
+    }
 //end getTheInput()
